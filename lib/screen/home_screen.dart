ec0e22ca4b80b9070bc4baf459e0f816fa9f1b20
siwoo Lee <leeseewoo@gmail.com>
2024-05-25 12:28:56 +0900

    Done! : 240525 1PM

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,20 +1,56 @@
 import 'package:cf_tube/component/custom_youtube_player.dart';
 import 'package:cf_tube/model/video_model.dart';
+import 'package:cf_tube/repository/youtube_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super (key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.black,
-      body: CustomYoutubePlayer(
-        videoModel: VideoModel(
-          id: '3Ck42C2ZCb8',
-          title : '다트 언어 기본기 1시간만에 끝내기',
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          '코팩튜브',
         ),
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<List<VideoModel>>(
+        future: YoutubeRepository.getVideos(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return RefreshIndicator(
+            onRefresh: () async {
+              setState(() {});
+            },
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: snapshot.data!
+                  .map((e) => CustomYoutubePlayer(videoModel: e))
+                  .toList(),
+            ),
+          );
+        },
       ),
     );
   }
